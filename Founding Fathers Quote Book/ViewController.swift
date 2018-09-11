7ef5132d36acde1368e7e44a8adf5e4b1d95ea6d
andy schirmer <andrewschirmer3@gmail.com>
2018-09-11 08:43:10 -0600

    Initial Commit

---
+++ Founding Fathers Quote Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Founding Fathers Quote Book
+//
+//  Created by Andrew Schirmer on 9/11/18.
+//  Copyright © 2018 Andrew Schirmer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
